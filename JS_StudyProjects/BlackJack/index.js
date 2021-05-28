b3116c938ebd147c6650e1089c2796302723af91
Marcelo Cristo Fuhr <marcelinhofuhr@gmail.com>
2021-05-28 19:19:20 -0300

    second commit

--- JS_StudyProjects/BlackJack/index.js
+++ JS_StudyProjects/BlackJack/index.js
@@ -1,5 +1,5 @@
-let firstCard = Math.floor(Math.random() * 10) + 2
-let secondCard = Math.floor(Math.random() * 10) + 2
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard] //array para ir adicionando cada vez que pede uma nova carta
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,12 +9,19 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+function getRandomCard() {
+    return Math.floor(Math.random() * 10) + 2
+}
+
 function startGame() { //por questão de semântica, foi criado uma renderGame para aplicar no JS. A function startGame é apenas para dar o início do jogo.
     renderGame()
 }
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1] //ira adicionar as cartas no html 
-    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cartas: " //ira adicionar as cartas no html
+    for(let i = 0; i < cards.length; i++) { //function para adicionar as cartas no elemento Cartas do HTML
+        cardsEl.textContent += cards[i] + " "
+    } 
+    sumEl.textContent = "Total: " + sum
     if (sum <= 20) {
         message = ("Você gostaria de mais uma carta?") //apenas dando valores para a mensagem. Será mostrada no html logo abaixo, no messageEl.textContent
     } else if (sum === 21) {
@@ -29,7 +36,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let anotherCard = Math.floor(Math.random() * 10) + 2
+    let anotherCard = getRandomCard()
     sum += anotherCard //mudando o valor da variável sum, adicionando a nova carta
     cards.push(anotherCard) // adicionando a nova carta na array cards
     renderGame() //chamando a função novamente
